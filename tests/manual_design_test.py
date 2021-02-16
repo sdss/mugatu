bc558acd42735059277df8b314d1ece126bf1ba8
imedan <ilija.medan@gmail.com>
2021-02-16 10:15:39 -0500

    Use more appropriate JD in test

--- tests/manual_design_test.py
+++ tests/manual_design_test.py
@@ -17,7 +17,7 @@ def test_warn_manual():
     test a manual design with inputs that generate
     some warnings
     """
-    des = FPSDesign(3, 2459366, racen=297.33348791823, deccen=-11.2681218565069,
+    des = FPSDesign(3, 2459519.51, racen=297.33348791823, deccen=-11.2681218565069,
                     position_angle=24.188576, observatory='APO',
                     design_file='tests/manual_design_test.fits',
                     manual_design=True)

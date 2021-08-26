123b8eb5901f249d5fe8407cd8e6b6e3eaae1b21
imedan <ilija.medan@gmail.com>
2021-08-26 10:10:35 -0400

    Change test to use rsFieldAssignment file as input

--- tests/manual_design_test.py
+++ tests/manual_design_test.py
@@ -6,7 +6,13 @@
 
 import os
 
+import numpy as np
+
 from mugatu.fpsdesign import FPSDesign
+from coordio.utils import radec2wokxy, wokxy2radec
+from astropy.io import fits
+from astropy.time import Time
+from coordio.exceptions import CoordinateError
 
 
 def test_warn_manual():
@@ -14,13 +20,15 @@ def test_warn_manual():
     test a manual design with inputs that generate
     some warnings
     """
-    des = FPSDesign(3, 2459519.51, racen=297.33348791823, deccen=-11.2681218565069,
-                    position_angle=24.188576, observatory='APO',
-                    design_file='tests/manual_design_test.fits',
-                    manual_design=True)
+    des = FPSDesign(design_pk=-1,
+                    obsTime=2459410.7920949073,
+                    design_file='rsFieldAssignments-test-designmode-2-apo-1.fits',
+                    manual_design=True,
+                    exp=1)
 
     des.build_design_manual()
     des.validate_design()
+    print(len(des.targets_collided))
 
 
 if __name__ == "__main__":

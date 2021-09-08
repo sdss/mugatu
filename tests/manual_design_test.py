2b050fe8eaadd8e6737cc773bfe1d2f50d9c53e0
imedan <ilija.medan@gmail.com>
2021-09-08 17:37:50 -0400

    Edit script so it can apply to rsFieldAssignment file at Utah

--- tests/manual_design_test.py
+++ tests/manual_design_test.py
@@ -5,31 +5,76 @@
 # @Copyright: Ilija Medan
 
 import os
+import sys
+import argparse
+from astropy.io import fits
 
-import numpy as np
+import sdss_access.path
 
 from mugatu.fpsdesign import FPSDesign
-from coordio.utils import radec2wokxy, wokxy2radec
-from astropy.io import fits
-from astropy.time import Time
-from coordio.exceptions import CoordinateError
+import robostrategy.obstime as obstime
+import coordio.time
 
 
-def test_warn_manual():
+def test_warn_manual(file, exp, obsTime, observatory):
     """
     test a manual design with inputs that generate
     some warnings
     """
     des = FPSDesign(design_pk=-1,
-                    obsTime=2459410.7920949073,
-                    design_file='rsFieldAssignments-test-designmode-2-apo-1.fits',
+                    obsTime=obsTime,
+                    observatory=observatory.upper(),
+                    design_file=file,
                     manual_design=True,
-                    exp=1)
+                    exp=exp)
 
     des.build_design_manual()
     des.validate_design()
-    print(len(des.targets_collided))
 
 
 if __name__ == "__main__":
-    test_warn_manual()
+    sdss_path = sdss_access.path.Path(release='sdss5')
+
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(sys.argv[0]),
+        description='Read Robostratgey design file to mugatu')
+
+    parser.add_argument('-p', '--plan', dest='plan',
+                        type=str, help='name of plan', required=True)
+    parser.add_argument('-o', '--observatory', dest='observatory',
+                        type=str, help='apo or lco',
+                        choices=['apo', 'lco'], required=True)
+    parser.add_argument('-f', '--fieldid', dest='fieldid',
+                        type=int, help='fieldid',
+                        required=True)
+    parser.add_argument('-e', '--exp', dest='exp',
+                        type=int, help='exposure of design',
+                        required=True)
+    parser.add_argument('-l', '--loc', dest='loc',
+                        type=str, help='local or utah',
+                        choices=['local', 'utah'], required=True)
+
+    args = parser.parse_args()
+    plan = args.plan
+    observatory = args.observatory
+    fieldid = args.fieldid
+    exp = args.exp
+    loc = args.loc
+
+    if loc == 'local':
+        file = 'rsFieldAssignments-%s-%s-%d.fits' % (plan, observatory, fieldid)
+    else:
+        file = sdss_path.full('rsFieldAssignments',
+                              plan=plan,
+                              observatory=observatory,
+                              fieldid=fieldid)
+        if 'sas' in file:
+            file = file[:32] + 'sdss50' + file[44:]
+
+    head = fits.open(file)[0].header
+    racen = head['RACEN']
+    ot = obstime.ObsTime(observatory=observatory.lower())
+    obsTime = coordio.time.Time(ot.nominal(lst=racen)).jd
+
+    test_warn_manual(file=file, exp=exp, obsTime=obsTime,
+                     observatory=observatory)

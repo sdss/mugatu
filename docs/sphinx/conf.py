93274f5e4ea92ebed5de49cb10c4f054ec971ec2
imedan <ilija.medan@gmail.com>
2022-06-09 14:45:11 -0400

    ignore sdsddb in doc build

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -123,7 +123,7 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3.6', None),
                        'astropy': ('http://docs.astropy.org/en/latest', None),
                        'numpy': ('http://docs.scipy.org/doc/numpy/', None)}
 
-autodoc_mock_imports = ['_tkinter']
+autodoc_mock_imports = ['_tkinter', 'sdssdb']
 autodoc_member_order = 'groupwise'
 
 napoleon_use_rtype = False
